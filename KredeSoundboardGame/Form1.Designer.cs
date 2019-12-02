cca4a973ed76bbe1c6ecb89369be9e431be42c00
Ftmjgtde <55089706+Ftmjgtde@users.noreply.github.com>
2019-12-02 22:48:05 +0100

    indsæt

---
+++ KredeSoundboardGame/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace KredeSoundboardGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Neger = new System.Windows.Forms.Button();
+            this.Chernobog = new System.Windows.Forms.Button();
+            this.Wham = new System.Windows.Forms.Button();
+            this.OOHHH = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.DeviceList = new System.Windows.Forms.ListView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.STOP = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Neger
+            // 
+            this.Neger.Location = new System.Drawing.Point(8, 11);
+            this.Neger.Name = "Neger";
+            this.Neger.Size = new System.Drawing.Size(75, 23);
+            this.Neger.TabIndex = 0;
+            this.Neger.Tag = "wav";
+            this.Neger.Text = "Neger";
+            this.Neger.UseVisualStyleBackColor = true;
+            this.Neger.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Chernobog
+            // 
+            this.Chernobog.Location = new System.Drawing.Point(89, 11);
+            this.Chernobog.Name = "Chernobog";
+            this.Chernobog.Size = new System.Drawing.Size(75, 23);
+            this.Chernobog.TabIndex = 1;
+            this.Chernobog.Tag = "wav";
+            this.Chernobog.Text = "Chernobog";
+            this.Chernobog.UseVisualStyleBackColor = true;
+            this.Chernobog.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Wham
+            // 
+            this.Wham.Location = new System.Drawing.Point(170, 11);
+            this.Wham.Name = "Wham";
+            this.Wham.Size = new System.Drawing.Size(75, 23);
+            this.Wham.TabIndex = 2;
+            this.Wham.Tag = "wav";
+            this.Wham.Text = "Wham";
+            this.Wham.UseVisualStyleBackColor = true;
+            this.Wham.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OOHHH
+            // 
+            this.OOHHH.Location = new System.Drawing.Point(251, 11);
+            this.OOHHH.Name = "OOHHH";
+            this.OOHHH.Size = new System.Drawing.Size(75, 23);
+            this.OOHHH.TabIndex = 3;
+            this.OOHHH.Tag = "wav";
+            this.OOHHH.Text = "OOHHH";
+            this.OOHHH.UseVisualStyleBackColor = true;
+            this.OOHHH.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(8, 40);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(89, 40);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(170, 40);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(251, 40);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(8, 69);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(89, 69);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(170, 69);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(251, 69);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // DeviceList
+            // 
+            this.DeviceList.HideSelection = false;
+            this.DeviceList.Location = new System.Drawing.Point(8, 98);
+            this.DeviceList.Name = "DeviceList";
+            this.DeviceList.Size = new System.Drawing.Size(318, 192);
+            this.DeviceList.TabIndex = 12;
+            this.DeviceList.UseCompatibleStateImageBehavior = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(348, 17);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(157, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Play on own speaker aswell";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // STOP
+            // 
+            this.STOP.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.STOP.Location = new System.Drawing.Point(332, 98);
+            this.STOP.Name = "STOP";
+            this.STOP.Size = new System.Drawing.Size(186, 192);
+            this.STOP.TabIndex = 14;
+            this.STOP.Text = "STOP";
+            this.STOP.UseVisualStyleBackColor = true;
+            this.STOP.Click += new System.EventHandler(this.STOP_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 302);
+            this.Controls.Add(this.STOP);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.DeviceList);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.OOHHH);
+            this.Controls.Add(this.Wham);
+            this.Controls.Add(this.Chernobog);
+            this.Controls.Add(this.Neger);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Neger;
+        private System.Windows.Forms.Button Chernobog;
+        private System.Windows.Forms.Button Wham;
+        private System.Windows.Forms.Button OOHHH;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.ListView DeviceList;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button STOP;
+    }
+}
+
---
+++ KredeSoundboardGame/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+using NAudio;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+
+
+namespace KredeSoundboardGame
+{
+    public partial class Form1 : Form
+    {
+        NAudio.Wave.WaveOut sourceStream;
+        NAudio.Wave.DirectSoundOut output;
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<NAudio.Wave.WaveOutCapabilities> sources = new List<NAudio.Wave.WaveOutCapabilities>();
+
+            for (int i = 0; i < NAudio.Wave.WaveOut.DeviceCount; i++)
+            {
+                sources.Add(NAudio.Wave.WaveOut.GetCapabilities(i));
+            }
+
+            DeviceList.Items.Clear();
+
+            foreach (var source in sources)
+            {
+                ListViewItem item = new ListViewItem(source.ProductName);
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, source.Channels.ToString()));
+                DeviceList.Items.Add(item);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string buttonTag = Convert.ToString((sender as Button).Tag);
+            string buttonName = (sender as Button).Name;
+            NAudio.Wave.WaveFileReader wave = new NAudio.Wave.WaveFileReader(buttonName + "." + buttonTag);
+
+            if (checkBox1.Checked)
+            {
+                output = new NAudio.Wave.DirectSoundOut();
+                output.Init(new NAudio.Wave.WaveChannel32(wave));
+                output.Play();
+            }
+            
+            if (DeviceList.SelectedItems.Count == 0) return;
+            int deviceNumber = DeviceList.SelectedItems[0].Index;
+            sourceStream = new NAudio.Wave.WaveOut();
+            sourceStream.DeviceNumber = deviceNumber;
+
+            sourceStream.Init(wave);
+            sourceStream.Play();
+        }
+
+        private void STOP_Click(object sender, EventArgs e)
+        {
+            if (sourceStream != null)
+            {
+                sourceStream.Dispose();
+                sourceStream = null;
+            }
+            if (output != null)
+            {
+                output.Dispose();
+                output = null;
+            }
+
+        }
+    }
+}
